3fa2a1210ebc1eeced366ac1f809fdba4b9c6e70
Rafal Bednarz <raf.bednarz@gmail.com>
2019-07-13 10:20:31 +0200

    first commit

---
+++ src/main/java/com/example/restclient/Main.java
@@ -0,0 +1,9 @@
+package com.example.restclient;
+
+import org.springframework.web.client.RestTemplate;
+
+public class Main {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+    }
+}
